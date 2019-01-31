de9f443fd82bc37577c6542c70331cc82d6b0d48
Hojun Hwang <voicihojun@gmail.com>
2019-01-31 11:25:50 -0500

    premier commit
    changer le code un peu

--- app/src/main/java/ift2905/net/ift2905_dev1/MainActivity.java
+++ app/src/main/java/ift2905/net/ift2905_dev1/MainActivity.java
@@ -2,12 +2,29 @@ package ift2905.net.ift2905_dev1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
+
+    //Chronometer chronometer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
+        button.setText("hojun");
+        textView.setText("hhohohoh");
+
+
     }
 }
