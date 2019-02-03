cf3dd7eecbb65f9e8aa0e0b177303ba799198035
Yifu Zhou <45948702+zhouyifu@users.noreply.github.com>
2019-02-02 20:41:27 -0500

    runnable on phone
    preset delay : 1000 millis

--- app/src/main/java/ift2905/net/ift2905_dev1/MainActivity.java
+++ app/src/main/java/ift2905/net/ift2905_dev1/MainActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.os.Handler;
+
 
 public class MainActivity extends AppCompatActivity {
     Button but;
+    Handler timer;
 
     boolean is_blinking;
-
+    boolean is_bright;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +20,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         but = (Button)findViewById(R.id.button);
+
+        timer = new Handler();
         but.setOnClickListener(buttonListener);
 
         but.setBackgroundColor(getResources().getColor(R.color.neutral));
+        is_blinking = false;
+        is_bright = false;
     }
 
     View.OnClickListener buttonListener = new View.OnClickListener(){
@@ -34,11 +41,27 @@ public class MainActivity extends AppCompatActivity {
                 but.setBackgroundColor(getResources().getColor(R.color.neutral));
             }else{
                 // Le clignotement est désactivé: L'activer
-                but.setText(R.string.desactiver);
+                but.setText(R.string.message);
                 is_blinking = true;
 
                 timer_listener.run();
             }
+        }
+    };
+
+    //code de demo1
+    Runnable timer_listener = new Runnable() {
+        @Override
+        public void run() {
+            if(is_bright){
+                but.setBackgroundColor(getResources().getColor(R.color.neutral));
+                is_bright = false;
+            }else{
+                but.setBackgroundColor(getResources().getColor(R.color.bright));
+                is_bright = true;
+            }
+
+            timer.postDelayed(timer_listener, 1000);
         }
     };
 }
