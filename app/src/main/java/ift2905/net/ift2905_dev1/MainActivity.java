92fe7448426f12c574ce4f76a87cfc75c0a528d9
Yifu Zhou <45948702+zhouyifu@users.noreply.github.com>
2019-02-03 09:45:36 -0500

    Revert "Essai compteur"
    This reverts commit 7dbc82d5e235fb06a0926acd11a9be18fee03820.

--- app/src/main/java/ift2905/net/ift2905_dev1/MainActivity.java
+++ app/src/main/java/ift2905/net/ift2905_dev1/MainActivity.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.os.Handler;
-import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
     Button but;
     Handler timer;
-    TextView tv2;
+
     boolean is_blinking;
     int compter;
 
@@ -20,14 +20,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         but = (Button)findViewById(R.id.button);
-        tv2 = (TextView)findViewById(R.id.textView2);
 
         timer = new Handler();
         but.setOnClickListener(buttonListener);
 
         but.setBackgroundColor(getResources().getColor(R.color.neutral));
         is_blinking = false;
-        compter = 1;
+        compter = 0;
     }
 
     View.OnClickListener buttonListener = new View.OnClickListener(){
@@ -37,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
                 // Le clignotement est activé: Le désactiver
                 but.setText(R.string.activer2);
                 is_blinking = false;
-                tv2.setText("Essai "+compter+" de 5");
+
                 timer.removeCallbacks(timer_listener);
                 but.setBackgroundColor(getResources().getColor(R.color.neutral));
             }else{
@@ -56,14 +55,13 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
             if(compter<=5){
                 but.setBackgroundColor(getResources().getColor(R.color.yellow));
-
                 compter ++;
             }else{
                 but.setBackgroundColor(getResources().getColor(R.color.yellow));
                 but.setText(R.string.message);
             }
 
-            timer.postDelayed(timer_listener, 3000);
+            timer.postDelayed(timer_listener, 1000);
         }
     };
 }
